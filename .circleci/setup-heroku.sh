08810d76932983a079e340577ed7c3ede2da075f
Taichi <taichi0529@asaichi.co.jp>
2017-08-14 21:05:59 +0900

    update .circleci/config.yml

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# TODO config.ymlの方の環境変数で設定出来るようにする。
 git remote add heroku https://git.heroku.com/memoo.git
 # Add heroku.com to the list of known hosts
 ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
