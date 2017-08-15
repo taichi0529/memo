6bd5df5ca45ab6e15bee23260e730cb586499088
Taichi <taichi0529@asaichi.co.jp>
2017-08-15 16:35:54 +0900

    update .circleci/config.yml

--- .circleci/create-artifacts.sh
+++ .circleci/create-artifacts.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
-FILENAME="build_"`date -u +"%Y%m%d%H%M%S"`".tar.gz";
+TIMESTAMP=`date -u +"%Y%m%d%H%M%S"`
+FILENAME="build_${TIMESTAMP}.tar.gz"
+FILENAME_NOMAP="build_${TIMESTAMP}-nomap.tar.gz"
 cd ~/repo/
 mkdir artifacts
 tar zcf  ${FILENAME} build/
 mv ${FILENAME} artifacts/
+rm -f build/static/css/*.map
+rm -f build/static/js/*.map
+tar zcf  ${FILENAME_NOMAP} build/
+mv ${FILENAME_NOMAP} artifacts/
 
